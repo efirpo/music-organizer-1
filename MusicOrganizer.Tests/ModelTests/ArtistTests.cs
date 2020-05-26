3019d11f8c70d78123f2e6ff6dacf4ca4b9552f7
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-26 14:01:09 -0700

    add tests for artist and song tables and objects

--- MusicOrganizer.Tests/ModelTests/ArtistTests.cs
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -18,31 +18,60 @@ namespace MusicOrganizer.Tests
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=music_organizer_test;";
     }
 
-    // [TestMethod]
-    // public void ArtistConstructor_CreateArtistInstance_Object()
-    // {
-    //   Artist newArtist = new Artist("P!ATD", "Pop Punk");
-    //   Assert.AreEqual(typeof(Artist), newArtist.GetType());
-    // }
+    [TestMethod]
+    public void Equals_ReturnsTrueIfNamesAreTheSame_Artist()
+    {
+      Artist artistOne = new Artist("David Bowie", "Magic Space Rock");
+      Artist artistTwo = new Artist("David Bowie", "Magic Space Rock");
 
-    // [TestMethod]
-    // public void GetName_ReturnsName_String()
-    // {
-    //   string name = "P!ATD";
-    //   Artist newArtist = new Artist(name, "Pop Punk");
-    //   string result = newArtist.Name;
-    //   Assert.AreEqual(name, result);
-    // }
+      Assert.AreEqual(artistOne, artistTwo);
+    }
 
-    // [TestMethod]
-    // public void GetGenre_ReturnsGenre_String()
-    // {
-    //   string name = "P!ATD";
-    //   string genre = "Pop Punk";
-    //   Artist newArtist = new Artist(name, genre);
-    //   string result = newArtist.Genre;
-    //   Assert.AreEqual(genre, result);
-    // }
+    [TestMethod]
+
+    public void Save_SavesToDatabase_ArtistList()
+    {
+      Artist testArtist = new Artist("David Bowie", "Magic Space Rock");
+
+      testArtist.Save();
+      List<Artist> result = Artist.GetAll();
+      List<Artist> testList = new List<Artist>{testArtist};
+
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsArtists_ArtistList()
+    {
+      string name01 = "David Bowie";
+      string genre01 = "Magic Space Rock";
+      string name02 = "Thom Yorke";
+      string genre02 = "Pretentious Space Rock";
+      Artist newArtist01 = new Artist(name01, genre01);
+      newArtist01.Save();
+      Artist newArtist02 = new Artist(name02, genre02);
+      newArtist02.Save();
+
+      List<Artist> newList = new List<Artist> {newArtist01, newArtist02};
+
+      List<Artist> result = Artist.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectArtistFromDatabase_Artist()
+    {
+      Artist newArtist = new Artist("David Bowie", "Magic Space Rock");
+      newArtist.Save();
+      Artist newArtist2 = new Artist("Thom Yorke", "Pretentious Space Rock");
+      newArtist2.Save();
+
+      Artist foundArtist = Artist.Find(newArtist.Id);
+
+      Assert.AreEqual(newArtist, foundArtist);
+    }
 
     [TestMethod]
     public void GetAll_ReturnsEmptyListFromDatabase_ArtistList()
--- MusicOrganizer.Tests/ModelTests/SongTests.cs
+++ MusicOrganizer.Tests/ModelTests/SongTests.cs
@@ -27,6 +27,24 @@ namespace MusicOrganizer.Tests
       CollectionAssert.AreEqual(newList, result);
     }
 
+    [TestMethod]
+    public void Equals_ReturnsTrueIfTitlesAreTheSame_Song()
+    {
+      Song firstSong = new Song("Heaven", "Now or Never", 2020);
+      Song secondSong = new Song("Heaven", "Now or Never", 2020);
+      Assert.AreEqual(firstSong, secondSong);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_SongList()
+    {
+      Song testSong = new Song("Ghost", "Forever", 2014);
+      testSong.Save();
+      List<Song> result = Song.GetAll();
+      List<Song> testList = new List<Song> {testSong};
+      CollectionAssert.AreEqual(testList, result);
+    }
+
     // [TestMethod]
     // public void SongConstructor_CreatesInstanceOfSong_Song()
     // {
@@ -52,19 +70,49 @@ namespace MusicOrganizer.Tests
     //   Assert.AreEqual(title, result);
     // }
 
-    // [TestMethod]
-    //   public void GetProperties_ReturnSongProperties_String()
-    // {
-    //   string title = "Skyfall";
-    //   string album = "James Bond";
-    //   int year = 2011;
-    //   Song newSong = new Song(title, album, year);
-    //   string titleResult = newSong.Title;
-    //   string albumResult = newSong.Album;
-    //   int yearResult = newSong.Year;
-    //   Assert.AreEqual(title, titleResult);
-    //   Assert.AreEqual(album, albumResult);
-    //   Assert.AreEqual(year, yearResult);      
-    // }
+    [TestMethod]
+    public void GetProperties_ReturnSongProperties_String()
+    {
+      string title = "Skyfall";
+      string album = "James Bond";
+      int year = 2011;
+      Song newSong = new Song(title, album, year);
+      newSong.Save();
+      string titleResult = newSong.Title;
+      string albumResult = newSong.Album;
+      int yearResult = newSong.Year;
+      Assert.AreEqual(title, titleResult);
+      Assert.AreEqual(album, albumResult);
+      Assert.AreEqual(year, yearResult);      
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsSongs_SongList()
+    {
+      string title = "Skyfall";
+      string album = "James Bond";
+      int year = 2011;
+      Song song1 = new Song(title, album, year);
+      song1.Save();
+      Song testSong = new Song("Ghost", "Forever", 2014);
+      testSong.Save();
+      List<Song> newList = new List<Song> {song1, testSong};
+      List<Song> result = Song.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectSongFromDatabase_Songs()
+    {
+      string title = "Skyfall";
+      string album = "James Bond";
+      int year = 2011;
+      Song song1 = new Song(title, album, year);
+      song1.Save();
+      Song testSong = new Song("Ghost", "Forever", 2014);
+      testSong.Save();
+      Song foundSong = Song.Find(song1.Id);
+      Assert.AreEqual(song1, foundSong);
+    }
   } 
 }
